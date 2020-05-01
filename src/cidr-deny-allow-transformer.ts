e5c3f8521ee72871171aadd515e7ab0e02b00086
Thomas Krause <thomas.krause@gmail.com>
2020-05-01 15:49:15 +0200

    Bootstrapped the project.

---
+++ src/cidr-deny-allow-transformer.ts
@@ -0,0 +1,98 @@
+import { cidr, ip } from 'node-cidr/dist'
+import { NumberInterval } from './number-interval'
+
+export default class CidrAllowDenyTransformer {
+  static findAllowedCidrs(fullCidr: string, denyCidrs: string[]): string[] {
+    const fullInterval = CidrAllowDenyTransformer.cidrToInterval(fullCidr)
+    const deniedIntervals = denyCidrs.map(deniedCidr =>
+      CidrAllowDenyTransformer.cidrToInterval(deniedCidr)
+    )
+
+    const allowedIntervals = deniedIntervals.reduce(
+      (currentIntervals, deny) =>
+        ([] as NumberInterval[]).concat(
+          ...currentIntervals.map(currentInterval => currentInterval.minus(deny))
+        ),
+      [fullInterval]
+    )
+
+    return ([] as string[]).concat(
+      ...allowedIntervals.map(interval =>
+        CidrAllowDenyTransformer.ipRangeToCidrs(
+          ip.toString(interval.start),
+          ip.toString(interval.end)
+        )
+      )
+    )
+  }
+
+  private static cidrToInterval(cidrStr: string): NumberInterval {
+    const range = cidr.toIntRange(cidrStr)
+    return new NumberInterval(range[0], range[1])
+  }
+
+  private static bytesToInt32(bytes: number[]): number {
+    return ((bytes[0] * 256 + bytes[1]) * 256 + bytes[2]) * 256 + bytes[3]
+  }
+
+  private static int32ToBytes(int32: number): number[] {
+    return [
+      (int32 >>> 24) & 0xff,
+      (int32 >>> 16) & 0xff,
+      (int32 >>> 8) & 0xff,
+      (int32 >>> 0) & 0xff
+    ]
+  }
+
+  private static ipToIp32(ipAddress: string): number | undefined {
+    const matches = ipAddress.match(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/)
+    if (matches) {
+      const ipBytes = []
+      for (let index = 1; index < matches.length; index++) {
+        const ipByte = Number(matches[index])
+        if (ipByte >= 0 && ipByte <= 255) {
+          ipBytes.push(ipByte)
+        }
+      }
+      if (ipBytes.length === 4) {
+        return CidrAllowDenyTransformer.bytesToInt32(ipBytes)
+      }
+    }
+    return undefined
+  }
+
+  private static ip32ToIp(ip32: number): string | undefined {
+    return isFinite(ip32)
+      ? CidrAllowDenyTransformer.int32ToBytes(ip32 & 0xffffffff).join('.')
+      : undefined
+  }
+
+  private static maxBlock(ip32: number): number {
+    let block = 32
+    while (block > 0) {
+      if ((ip32 >>> (32 - block)) & 0x00000001) {
+        break
+      } else {
+        block--
+      }
+    }
+    return block
+  }
+
+  private static ipRangeToCidrs(firstIp: string, lastIp: string): string[] {
+    let firstIp32 = CidrAllowDenyTransformer.ipToIp32(firstIp)
+    const lastIp32 = CidrAllowDenyTransformer.ipToIp32(lastIp)
+
+    const cidrs = []
+    if (firstIp32 && lastIp32 && firstIp32 <= lastIp32) {
+      while (lastIp32 >= firstIp32) {
+        const maxSize = CidrAllowDenyTransformer.maxBlock(firstIp32)
+        const maxDiff = 32 - Math.floor(Math.log(lastIp32 - firstIp32 + 1) / Math.log(2))
+        const size = Math.max(maxSize, maxDiff)
+        cidrs.push(CidrAllowDenyTransformer.ip32ToIp(firstIp32) + '/' + size)
+        firstIp32 += Math.pow(2, 32 - size)
+      }
+    }
+    return cidrs
+  }
+}
---
+++ test/cidr-deny-allow-transformer.spec.ts
@@ -0,0 +1,38 @@
+import CidrAllowDenyTransformer from '../src/cidr-deny-allow-transformer'
+
+describe('CidrAllowDenyTransformer', () => {
+  const fullCidr = '192.160.0.0/12'
+  const deniedCidrsWithinRange = ['192.162.0.0/18', '192.172.0.0/23', '192.172.56.0/21']
+  const deniedCidrsOutsideOfRange = ['192.150.0.0/18', '192.176.0.0/23', '192.176.42.0/21']
+  const expectedAllowedCidrs = [
+    '192.160.0.0/15',
+    '192.162.64.0/18',
+    '192.162.128.0/17',
+    '192.163.0.0/16',
+    '192.164.0.0/14',
+    '192.168.0.0/14',
+    '192.172.2.0/23',
+    '192.172.4.0/22',
+    '192.172.8.0/21',
+    '192.172.16.0/20',
+    '192.172.32.0/20',
+    '192.172.48.0/21',
+    '192.172.64.0/18',
+    '192.172.128.0/17',
+    '192.173.0.0/16',
+    '192.174.0.0/15'
+  ]
+
+  it('should calculate the allowed CIDRs correctly', () => {
+    const allowedCidrs = CidrAllowDenyTransformer.findAllowedCidrs(fullCidr, deniedCidrsWithinRange)
+    expect(allowedCidrs).toEqual(expectedAllowedCidrs)
+  })
+
+  it('should not change the CIDRs when a completely different full range is specified', () => {
+    const allowedCidrs = CidrAllowDenyTransformer.findAllowedCidrs(
+      fullCidr,
+      deniedCidrsOutsideOfRange
+    )
+    expect(allowedCidrs).toEqual([fullCidr])
+  })
+})
